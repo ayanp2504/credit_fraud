a730b2db74071c4f4759d917ff038c88bb86b4c9
Ayan Pinjara <ayanpinjara@gmail.com>
2024-01-06 12:46:10 -0600

    Cookiecutter template initialized and train.txt created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This will predict credit card fraud using ML.',
+    author='Ayan Pinjara',
+    license='',
+)
